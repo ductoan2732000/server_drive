f713631db57f764b4914e330ac421e0a4747644a
Trần Toản <ductoan2732000@gmail.com>
2021-11-17 19:49:44 +0700

    upload file lên local của server trước sau đó mới đẩy lên drive

--- multer/multer.js
+++ multer/multer.js
@@ -1,24 +1,15 @@
 // tdtoan : 14.11.2021 sử dụng thư viện multer để lưu file gửi từ client lên
-// let multer = require("multer");
-// let storage = multer.diskStorage({
-//   destination: (req, file, callback) => {
-//     callback(null, "file_uploads");
-//   },
-//   filename: (req, file, callback) => {
-//     const { originalname } = file;
+let multer = require("multer");
+let storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "file_uploads");
+  },
+  filename: (req, file, callback) => {
+    const { originalname } = file;
 
-//     callback(null, originalname);
-//   },
-// });
-// let upload = multer({ storage });
-// module.exports = upload;
+    callback(null, originalname);
+  },
+});
+let upload = multer({ storage });
+module.exports = upload;
 
-
-const express = require("express");
-const todoRouter from "./todo.routes";
-
-const apiRoute = express();
-
-apiRoute.use("/todo", todoRouter);
-
-export default apiRoute;
--- routes/router.js
+++ routes/router.js
@@ -1,20 +1,22 @@
-// var express = require("express");
-// var router = express.Router();
-// const multer = require("../multer/multer");
-// router.get("/", function (req, res, next) {
-//   res.send("trandcoan");
-// });
-// router.post("/upload", multer.array("avatar"), (req, res) => {
-//   return res.json({ status: "OK", uploaded: req.files.length });
-// });
-// module.exports = router;
+var express = require("express");
+var router = express.Router();
+const multer = require("../multer/multer");
+/**
+ * tdtoan 17.11.2021 
+ * hàm get data từ server google drive
+ */
+router.get("/", function (req, res, next) {
+  res.send("trandcoan");
+});
+/**
+ * tdtoan 17.11.2021
+ * hàm post data lên google drive
+ */
+router.post("/upload", multer.array("avatar"), (req, res) => {
+  return res.json({ status: "OK", uploaded: req.files.length });
+});
+module.exports = router;
 
 
 
-const express = require("express");
-const todoRouter = require("./todo.routes");
 
-const apiRoute = express();
-
-apiRoute.use("/todo", todoRouter);
-module.exports = apiRoute;
